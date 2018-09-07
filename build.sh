d7af1031868cb048f202fde2ccb2aa79ba289d9e
Ruijie.Fu <furuijie@gmail.com>
2018-09-07 10:40:37 -0700

    add build script

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#
+# Script to build the whole project
+#
+
+set -o pipefail
+
+export ROOT="${ROOT:-$(git rev-parse --show-toplevel)}"
+. $ROOT/scripts/exports.sh
+
+echo "Fetching secrets ..."
+$ROOT/scripts/fetch_secrets.sh
+
+
+./gradlew cleanIdea clean idea build
