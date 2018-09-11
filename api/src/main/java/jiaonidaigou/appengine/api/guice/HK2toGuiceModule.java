20c5401e9d8e51a12bd9778888cd68bfec48c0fe
Ruijie.Fu <furuijie@gmail.com>
2018-09-11 00:33:13 -0700

    minor

--- api/src/main/java/jiaonidaigou/appengine/api/guice/HK2toGuiceModule.java
+++ api/src/main/java/jiaonidaigou/appengine/api/guice/HK2toGuiceModule.java
@@ -1,8 +1,11 @@
 package jiaonidaigou.appengine.api.guice;
 
 import com.google.inject.Injector;
+import jiaonidaigou.appengine.api.interfaces.CronInterface;
 import jiaonidaigou.appengine.api.interfaces.MediaInterface;
 import jiaonidaigou.appengine.api.interfaces.PingInterface;
+import jiaonidaigou.appengine.api.interfaces.ShippingOrderInterface;
+import jiaonidaigou.appengine.api.interfaces.TaskQueueInterface;
 import org.glassfish.hk2.api.Factory;
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
 
@@ -17,8 +20,11 @@ public class HK2toGuiceModule extends AbstractBinder {
 
     @Override
     protected void configure() {
+        bindInterface(CronInterface.class);
         bindInterface(MediaInterface.class);
         bindInterface(PingInterface.class);
+        bindInterface(ShippingOrderInterface.class);
+        bindInterface(TaskQueueInterface.class);
     }
 
     private <T> void bindInterface(final Class<T> interfaceType) {
