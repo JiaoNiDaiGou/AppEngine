ee2363bd365436eadb00ddb8bb99ca08af7c64b2
Ruijie.Fu <furuijie@gmail.com>
2018-09-10 23:40:44 -0700

    minior

--- api/src/main/java/jiaonidaigou/appengine/api/access/storage/GcsClient.java
+++ api/src/main/java/jiaonidaigou/appengine/api/access/storage/GcsClient.java
@@ -1,8 +1,10 @@
 package jiaonidaigou.appengine.api.access.storage;
 
+import com.google.cloud.storage.BlobId;
 import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.HttpMethod;
 import com.google.cloud.storage.Storage;
+import org.apache.commons.lang3.StringUtils;
 import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,9 +14,12 @@ import java.util.concurrent.TimeUnit;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import static com.google.common.base.Preconditions.checkArgument;
+
 @Singleton
 public class GcsClient implements StorageClient {
     private static final Logger LOGGER = LoggerFactory.getLogger(StorageClient.class);
+    private static final String GS_SCHEME = "gs://";
 
     private final Storage storage;
 
@@ -24,26 +29,26 @@ public class GcsClient implements StorageClient {
     }
 
     @Override
-    public boolean exists(String path) {
-        return getMetadata(path) != null;
+    public boolean exists(final String path) {
+        return storage.get(blobId(path)).exists();
     }
 
     @Override
     public Metadata getMetadata(final String path) {
+
+
         return null;
     }
 
     @Override
     public byte[] read(final String path) {
-        BucketPath bucketPath = BucketPath.of(path);
-        return storage.readAllBytes(bucketPath.getBucket(), bucketPath.getObject());
+        return storage.readAllBytes(blobId(path));
     }
 
     @Override
     public void write(final String path, final String mediaType, byte[] bytes) {
-        BucketPath bucketPath = BucketPath.of(path);
         storage.create(
-                BlobInfo.newBuilder(bucketPath.getBucket(), bucketPath.getObject())
+                BlobInfo.newBuilder(blobId(path))
                         .setContentType(mediaType)
                         .build(),
                 bytes);
@@ -51,170 +56,45 @@ public class GcsClient implements StorageClient {
 
     @Override
     public void copy(final String fromPath, final String toPath) {
+        storage.copy(Storage.CopyRequest.of(blobId(fromPath), blobId(toPath)));
     }
 
     @Override
     public URL getSignedUploadUrl(final String path, final String mediaType, final DateTime expiration) {
-        BucketPath bucketPath = BucketPath.of(path);
         return storage.signUrl(
-                BlobInfo.newBuilder(bucketPath.getBucket(), bucketPath.getObject()).setContentType(mediaType).build(),
+                BlobInfo.newBuilder(blobId(path)).setContentType(mediaType).build(),
                 expiration.getMillis(),
                 TimeUnit.MILLISECONDS,
-                Storage.SignUrlOption.httpMethod(HttpMethod.POST),
-                Storage.SignUrlOption.withContentType(),
-                Storage.SignUrlOption.withMd5());
+                Storage.SignUrlOption.httpMethod(HttpMethod.POST)
+//                Storage.SignUrlOption.withContentType()
+        );
     }
 
     @Override
     public URL getSignedDownloadUrl(final String path, final String mediaType, final DateTime expiration) {
-        BucketPath bucketPath = BucketPath.of(path);
         return storage.signUrl(
-                BlobInfo.newBuilder(bucketPath.getBucket(), bucketPath.getObject()).setContentType(mediaType).build(),
+                BlobInfo.newBuilder(blobId(path)).setContentType(mediaType).build(),
                 expiration.getMillis(),
                 TimeUnit.MILLISECONDS,
-                Storage.SignUrlOption.httpMethod(HttpMethod.GET),
-                Storage.SignUrlOption.withContentType(),
-                Storage.SignUrlOption.withMd5());
+                Storage.SignUrlOption.httpMethod(HttpMethod.GET)
+//                Storage.SignUrlOption.withContentType()
+        );
     }
 
-//
-//    @Override
-//    public boolean exists(final String path) {
-//        meterOn(this.getClass());
-//        boolean toReturn;
-//        try {
-//            toReturn = gcsService.getMetadata(toGcsFilename(path)) != null;
-//        } catch (IOException e) {
-//            throw new InternalIOException(e);
-//        }
-//        meterOff();
-//        return toReturn;
-//    }
-//
-//    @Override
-//    public Metadata getMetadata(final String path) {
-//        meterOn(this.getClass());
-//        GcsFileMetadata metadata;
-//        try {
-//            metadata = gcsService.getMetadata(toGcsFilename(path));
-//        } catch (IOException e) {
-//            throw new InternalIOException(e);
-//        }
-//        if (metadata == null) {
-//            meterOff();
-//            return null;
-//        }
-//        meterOff();
-//        return Metadata.builder()
-//                .withPath(path)
-//                .withLastModified(new DateTime(metadata.getLastModified()))
-//                .withLength(metadata.getLength())
-//                .build();
-//    }
-//
-//    @Override
-//    public byte[] read(String path) {
-//        return new byte[0];
-//    }
-//
-//    @Override
-//    public void write(String path, byte[] bytes) {
-//
-//    }
-//
-//    @Override
-//    public InputStream inputStream(final String path) {
-//        meterOn(this.getClass());
-//        GcsFilename filename = toGcsFilename(path);
-//        LOGGER.info("InputStream from {}", filename);
-//        GcsInputChannel channel = gcsService.openPrefetchingReadChannel(filename, 0, BUFFER_SIZE);
-//        meterOff();
-//        return Channels.newInputStream(channel);
-//    }
-//
-//    @Override
-//    public OutputStream outputStream(final String path, final MediaType mediaType) {
-//        meterOn(this.getClass());
-//        GcsFilename filename = toGcsFilename(path);
-//        LOGGER.info("OutputStream from {}", filename);
-//        GcsFileOptions options = new GcsFileOptions.Builder()
-//                .mimeType(mediaType.toString())
-//                .build();
-//        GcsOutputChannel channel;
-//        try {
-//            channel = gcsService.createOrReplace(filename, options);
-//        } catch (IOException e) {
-//            throw new InternalIOException(e);
-//        }
-//        meterOff();
-//        return Channels.newOutputStream(channel);
-//    }
-//
-//    @Override
-//    public void copy(String fromPath, String toPath) {
-//        meterOn(this.getClass());
-//        GcsFilename fromFilename = toGcsFilename(fromPath);
-//        GcsFilename toFilename = toGcsFilename(toPath);
-//        try {
-//            gcsService.copy(fromFilename, toFilename);
-//        } catch (IOException e) {
-//            throw new InternalIOException(e);
-//        }
-//        meterOff();
-//    }
-//
-//    @Override
-//    public String getSignedUploadUrl(final String path, final MediaType mediaType, final DateTime expiration) {
-//        return getSignedUrl(HTTPMethod.PUT, toGcsFilename(path), mediaType, expiration);
-//    }
-//
-//    @Override
-//    public String getSignedDownloadUrl(final String path, final MediaType mediaType, final DateTime expiration) {
-//        return getSignedUrl(HTTPMethod.GET, toGcsFilename(path), mediaType, expiration);
-//    }
-//
-//    private String getSignedUrl(final HTTPMethod httpMethod,
-//                                final GcsFilename filename,
-//                                final MediaType mediaType,
-//                                final DateTime expiration) {
-//        try {
-//            meterOn(this.getClass());
-//            String unsigned = stringToSign(httpMethod, filename, mediaType.toString(), expiration);
-//            String signature = sign(unsigned);
-//            meterOff();
-//            return String.format("%s/%s/%s?GoogleAccessId=%s&Expires=%d&Signature=%s",
-//                    BASE_URL,
-//                    filename.getBucketName(),
-//                    filename.getObjectName(),
-//                    appIdentityService.getServiceAccountName(),
-//                    expiration.getMillis() / 1000,
-//                    URLEncoder.encode(signature, UTF_8.name()));
-//        } catch (UnsupportedEncodingException e) {
-//            throw new InternalIOException(e);
-//        }
-//    }
-//
-//    private String sign(final String unsigned)
-//            throws UnsupportedEncodingException {
-//        // Note that the algorithm used by AppIdentity.signForApp() is "SHA256withRSA"
-//        AppIdentityService.SigningResult signingResult = appIdentityService.signForApp(unsigned.getBytes(UTF_8));
-//        return new String(Base64.encodeBase64(signingResult.getSignature(), false), UTF_8);
-//    }
-//
-//    private String stringToSign(final HTTPMethod httpMethod,
-//                                final GcsFilename filename,
-//                                final String contentType,
-//                                final DateTime expiration) {
-//        String contentMD5 = "";
-//        String canonicalizedExtensionHeaders = "";
-//        String canonicalizedResource = String.format("/%s/%s", filename.getBucketName(), filename.getObjectName());
-//        // expiration needs to be in seconds
-//        return String.format("%s%n%s%n%s%n%d%n%s%s",
-//                httpMethod,
-//                contentMD5,
-//                contentType,
-//                expiration.getMillis() / 1000,
-//                canonicalizedExtensionHeaders,
-//                canonicalizedResource);
-//    }
+    /**
+     * Path is in format of
+     * gcs://bucket_a/bucket_b/file.ext
+     * bucket is 'bucket_a/bucket_b'
+     * object is 'file.ext'
+     */
+    private static BlobId blobId(final String path) {
+        checkArgument(StringUtils.startsWithIgnoreCase(path, GS_SCHEME));
+        String pathWithoutScheme = path.substring(GS_SCHEME.length());
+        int firstSlash = StringUtils.indexOf(pathWithoutScheme, '/');
+        String bucket = StringUtils.substring(pathWithoutScheme, 0, firstSlash);
+        String object = StringUtils.substring(pathWithoutScheme, firstSlash + 1);
+        BlobId blobId = BlobId.of(bucket, object);
+        LOGGER.info("convert path {} to {}", path, blobId);
+        return blobId;
+    }
 }
--- tools/src/main/java/jiaonidaigou/appengine/tools/VerifyGcsClient2.java
+++ tools/src/main/java/jiaonidaigou/appengine/tools/VerifyGcsClient2.java
@@ -4,11 +4,6 @@ import jiaonidaigou.appengine.common.utils.Environments;
 import jiaonidaigou.appengine.tools.remote.ApiClient;
 import jiaonidaigou.appengine.wiremodel.entity.MediaObject;
 
-import java.io.DataOutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import static jiaonidaigou.appengine.tools.ResponseHandler.handle;
@@ -17,37 +12,37 @@ public class VerifyGcsClient2 {
     public static void main(String[] args) throws Exception {
         ApiClient client = new ApiClient(Environments.MAIN_VERSION_GAE_HOSTNAME);
 
-        // Get signed upload URL
-        Response response = client.newTarget("Get signed upload URL")
-                .path("/api/media/url/upload")
-                .queryParam("ext", "txt")
-                .request()
-                .get();
-        System.out.println("Get signed upload URL");
-        MediaObject uploadObject = handle(response, MediaObject.class);
-
-        final String mediaId = uploadObject.getId();
-
-        // Upload
-        URL uploadUrl = new URL(uploadObject.getSignedUploadUrl());
-        HttpURLConnection connection = (HttpURLConnection) uploadUrl.openConnection();
-        connection.setDoOutput(true);
-        connection.setRequestMethod("PUT");
-        try (DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream())) {
-            outputStream.writeBytes("some content");
-        }
-        handle(connection);
-
-        response = client.newTarget("Directly PUT signed URL", uploadObject.getSignedUploadUrl())
-                .request()
-                .put(Entity.entity("some file", MediaType.APPLICATION_OCTET_STREAM_TYPE));
-        System.out.println("\nPUT to GCS");
-        handle(response);
+//        // Get signed upload URL
+//        Response response = client.newTarget("Get signed upload URL")
+//                .path("/api/media/url/upload")
+//                .queryParam("ext", "txt")
+//                .request()
+//                .get();
+//        System.out.println("Get signed upload URL");
+//        MediaObject uploadObject = handle(response, MediaObject.class);
+//
+//        final String mediaId = uploadObject.getId();
+//
+//        // Upload
+//        URL uploadUrl = new URL(uploadObject.getSignedUploadUrl());
+//        HttpURLConnection connection = (HttpURLConnection) uploadUrl.openConnection();
+//        connection.setDoOutput(true);
+//        connection.setRequestMethod("PUT");
+//        try (DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream())) {
+//            outputStream.writeBytes("some content");
+//        }
+//        handle(connection);
+//
+//        response = client.newTarget("Directly PUT signed URL", uploadObject.getSignedUploadUrl())
+//                .request()
+//                .put(Entity.entity("some file", MediaType.APPLICATION_OCTET_STREAM_TYPE));
+//        System.out.println("\nPUT to GCS");
+//        handle(response);
 
         // Get signed download URL
-        response = client.newTarget("Get signed download URL")
+        Response response = client.newTarget("Get signed download URL")
                 .path("/api/media/url/download")
-                .queryParam("media_id", mediaId)
+                .queryParam("media_id", "temp.txt")
                 .request()
                 .get();
         System.out.println("\nGet signed download URL");
