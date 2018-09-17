641203b3aff13ff852de879ae3dc09d28eeab9a4
rfu <rfu@snapchat.com>
2018-09-17 12:04:25 -0700

    add customer parser

--- content-parser/src/main/java/jiaonidaigou/appengine/contentparser/Answers.java
+++ content-parser/src/main/java/jiaonidaigou/appengine/contentparser/Answers.java
@@ -2,12 +2,40 @@ package jiaonidaigou.appengine.contentparser;
 
 import com.google.common.base.MoreObjects;
 import com.google.common.collect.ImmutableList;
+import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Answers<T> implements Iterable<Answer<T>> {
+    @SafeVarargs
+    public static int weightedAverage(final Pair<Answer, Integer>... answersAndWeight) {
+        if (answersAndWeight.length == 0) {
+            return 0;
+        }
+        int sum = 0;
+        int totalWeight = 0;
+        for (Pair<Answer, Integer> pair : answersAndWeight) {
+            Answer answer = pair.getLeft();
+            int weight = pair.getRight();
+            sum += answer.hasTarget() ? answer.getConfidence() * weight : 0;
+            totalWeight += weight;
+        }
+        return sum / totalWeight;
+    }
+
+    public static <M> Answers<M> useInputIfNoAnswer(final Answers<M> answers,
+                                                    final String input) {
+        if (answers.size() != 0) {
+            return answers;
+        } else {
+            Answer<M> answerOfInput = new Answer<M>().setTarget(null, Conf.CONFIRMED)
+                    .setRawStringAfterExtraction(input);
+            return Answers.of(answerOfInput);
+        }
+    }
+
     private static final Answers NO_ANSWER = new Answers<>(ImmutableList.of());
 
     private final List<Answer<T>> results;
@@ -24,9 +52,8 @@ public class Answers<T> implements Iterable<Answer<T>> {
         return new Answers<>(ImmutableList.copyOf(answers));
     }
 
-    @SafeVarargs
-    public static <T> Answers of(final Answer<T>... answer) {
-        return new Answers<>(ImmutableList.copyOf(answer));
+    public static <T> Answers of(final Answer<T> answer) {
+        return new Answers<>(ImmutableList.of(answer));
     }
 
     public List<Answer<T>> getResults() {
---
+++ content-parser/src/main/java/jiaonidaigou/appengine/contentparser/CnCustomerContactParser.java
@@ -0,0 +1,89 @@
+package jiaonidaigou.appengine.contentparser;
+
+import jiaonidaigou.appengine.wiremodel.entity.Address;
+import jiaonidaigou.appengine.wiremodel.entity.Customer;
+import jiaonidaigou.appengine.wiremodel.entity.PhoneNumber;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static jiaonidaigou.appengine.contentparser.Answers.noAnswer;
+import static jiaonidaigou.appengine.contentparser.Answers.useInputIfNoAnswer;
+import static jiaonidaigou.appengine.contentparser.Answers.weightedAverage;
+
+public class CnCustomerContactParser implements Parser<Customer> {
+    private CnPeopleNameParser peopleNameParser;
+    private CnCellPhoneParser phoneParser;
+    private CnAddressParser addressParser;
+
+    public CnCustomerContactParser() {
+        addressParser = new CnAddressParser();
+        peopleNameParser = new CnPeopleNameParser();
+        phoneParser = new CnCellPhoneParser();
+    }
+
+    @Override
+    public Answers<Customer> parse(final String rawInput) {
+        if (rawInput == null) {
+            return Answers.noAnswer();
+        }
+
+        String input = rawInput.trim();
+
+        List<Answer<Customer>> customers = new ArrayList<>();
+
+        // Phone first
+        Answers<PhoneNumber> phoneAnswers = useInputIfNoAnswer(phoneParser.parse(input), input);
+        for (Answer<PhoneNumber> phoneAnswer : phoneAnswers) {
+            PhoneNumber phone = phoneAnswer.getTarget();
+            input = phoneAnswer.getRawStringAfterExtraction();
+
+            // People name next
+            Answers<String> nameAnswers = useInputIfNoAnswer(peopleNameParser.parse(input), input);
+            for (Answer<String> nameAnswer : nameAnswers) {
+                String name = nameAnswer.getTarget();
+                input = nameAnswer.getRawStringAfterExtraction();
+
+                // Address last
+                Answers<Address> addressAnswers = useInputIfNoAnswer(addressParser.parse(input), input);
+                for (Answer<Address> addressAnswer : addressAnswers) {
+                    Address address = addressAnswer.getTarget();
+                    input = addressAnswer.getRawStringAfterExtraction();
+
+                    if (phone == null && StringUtils.isBlank(name) && address == null) {
+                        continue;
+                    }
+
+                    Customer.Builder builder = Customer.newBuilder();
+                    if (phone != null) {
+                        builder.setPhone(phone);
+                    }
+                    if (StringUtils.isNotBlank(name)) {
+                        builder.setName(name);
+                    }
+                    if (address != null) {
+                        builder.addAddresses(address);
+                    }
+
+                    // Conf rule: name 20% phone 40% address 40%
+                    int conf = weightedAverage(
+                            Pair.of(nameAnswer, 2),
+                            Pair.of(phoneAnswer, 4),
+                            Pair.of(addressAnswer, 4));
+
+                    customers.add(new Answer<Customer>()
+                            .setTarget(builder.build(), conf)
+                            .setRawStringAfterExtraction(input));
+                }
+            }
+        }
+
+        if (customers.isEmpty()) {
+            return noAnswer();
+        }
+        customers.sort((a, b) -> Integer.compare(b.getConfidence(), a.getConfidence()));
+        return Answers.of(customers);
+    }
+}
