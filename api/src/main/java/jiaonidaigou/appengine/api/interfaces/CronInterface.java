460b37f44c373c73114d832bb764ad38752af391
rfu <rfu@snapchat.com>
2018-09-19 15:23:33 -0700

    minor

--- api/src/main/java/jiaonidaigou/appengine/api/interfaces/CronInterface.java
+++ api/src/main/java/jiaonidaigou/appengine/api/interfaces/CronInterface.java
@@ -1,15 +1,22 @@
 package jiaonidaigou.appengine.api.interfaces;
 
+import jiaonidaigou.appengine.api.access.taskqueue.TaskQueueClient;
 import jiaonidaigou.appengine.api.auth.Roles;
+import jiaonidaigou.appengine.api.tasks.SyncJiaoniCustomersTaskRunner;
+import jiaonidaigou.appengine.api.tasks.TaskMessage;
 import org.jvnet.hk2.annotations.Service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import static jiaonidaigou.appengine.api.access.taskqueue.TaskQueueClient.QueueName.HIGH_FREQUENCY;
 
 @Path("/cron")
 @Produces(MediaType.APPLICATION_JSON)
@@ -18,7 +25,22 @@ import javax.ws.rs.core.MediaType;
 public class CronInterface {
     private static final Logger LOGGER = LoggerFactory.getLogger(CronInterface.class);
 
+    private final TaskQueueClient taskQueueClient;
+
     @Inject
-    public CronInterface() {
+    public CronInterface(final TaskQueueClient taskQueueClient) {
+        this.taskQueueClient = taskQueueClient;
+    }
+
+    @Path("/syncJiaoniCustomers")
+    @GET
+    public Response syncJiaoniCustomers() {
+        taskQueueClient.submit(
+                HIGH_FREQUENCY,
+                TaskMessage.builder()
+                        .withHandler(SyncJiaoniCustomersTaskRunner.class)
+                        .build()
+        );
+        return Response.ok().build();
     }
 }
--- content-parser/src/test/java/jiaonidaigou/appengine/contentparser/CnCellPhoneParserTest.java
+++ content-parser/src/test/java/jiaonidaigou/appengine/contentparser/CnCellPhoneParserTest.java
@@ -3,6 +3,10 @@ package jiaonidaigou.appengine.contentparser;
 import jiaonidaigou.appengine.wiremodel.entity.PhoneNumber;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static jiaonidaigou.appengine.contentparser.AnswerMatchers.hasAnswers;
 import static jiaonidaigou.appengine.contentparser.AnswerMatchers.is;
 import static jiaonidaigou.appengine.contentparser.Conf.CONFIRMED;
@@ -23,5 +27,28 @@ public class CnCellPhoneParserTest {
                 is("13812345670", CONFIRMED),
                 is("13812345688", CONFIRMED)
         ));
+    }
+
+    @Test
+    public void testDD() {
+        List<Character> a = Arrays.asList('3', '4');
+        List<Character> b = Arrays.asList('5', '6');
+        int n = a.size() - 1;
+        int m = b.size() - 1;
+        int carry = 0;
+        List<Integer> result = new ArrayList<>(n + m);
+        int i = 0, j = 0;
+        for (i = n; i >= 0; --i) {
+            for (j = m; j >= 0; --j) {
+                int temp = (a.get(i) - '0') * (b.get(j) - '0');
+                result.set(i + j - 1, (carry + temp) % 10);
+                carry = temp / 10;
+            }
+        }
+        if (carry > 0) {
+            result.set(i + j - 1, carry);
+        }
+
+        System.out.println(result);
     }
 }
